6ac2a6fe11b560c8470054ca08abfa188a6a17f3
Vincent <lochenv@gmail.com>
2019-05-03 19:11:20 +0200

    Commit Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,37 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent}
+
+    // {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatIconModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatCardModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // <-- debugging purposes only
+        ),
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatIconModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        RouterModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
