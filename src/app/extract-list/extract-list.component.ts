de58f86f3966f7bd867db26c1aa9ad064c6c88c9
Vincent <lochenv@gmail.com>
2019-06-13 14:55:00 +0200

    new component

---
+++ src/app/extract-list/extract-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extract-list',
+  templateUrl: './extract-list.component.html',
+  styleUrls: ['./extract-list.component.scss']
+})
+export class ExtractListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
