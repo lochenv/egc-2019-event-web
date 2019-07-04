c76bc36a5bf8d6f741286dcebb5801badad0225f
Vincent Lochen <Vincent.Lochen@nbb.be>
2019-07-04 19:42:46 +0200

    =enhance registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,102 +5,94 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-    MAT_FORM_FIELD_DEFAULT_OPTIONS,
-    MatButtonModule,
-    MatCardModule, MatCheckboxModule, MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatMenuModule, MatProgressSpinnerModule,
-    MatSelectModule,
-    MatSortModule,
-    MatTableModule,
-    MatToolbarModule
+  MAT_FORM_FIELD_DEFAULT_OPTIONS, MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule,
+  MatIconModule, MatInputModule, MatMenuModule, MatProgressSpinnerModule, MatRadioModule, MatSelectModule, MatSortModule, MatTableModule,
+  MatToolbarModule
 } from '@angular/material';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
 import {RegistrationComponent} from './registration/registration.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {SignInService, SubscribersService, AuthenticationInterceptor, RegistrationService, ErrorLogoutInterceptor} from './shared';
+import {AuthenticationInterceptor, ErrorLogoutInterceptor, RegistrationService, SignInService, SubscribersService} from './shared';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './login/login.component';
-import {AuthGardService} from './shared/services/auth-gard.service';
 import {ExtractListComponent} from './extract-list/extract-list.component';
 import {FileSaverModule} from 'ngx-filesaver';
-import { ConfirmDownloadDialogComponent } from './confirm-download-dialog/confirm-download-dialog.component';
+import {ConfirmDownloadDialogComponent} from './confirm-download-dialog/confirm-download-dialog.component';
 
 const appRoutes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent,
-        // canActivate: [AuthGardService]
-    },
-    {
-        path: 'registration',
-        component: RegistrationComponent,
-        // canActivate: [AuthGardService]
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'extract-list',
-        component: ExtractListComponent
-    },
-    {
-        path: '**',
-        redirectTo: '/home'
-    }
+  {
+    path: 'home',
+    component: HomeComponent,
+    // canActivate: [AuthGardService]
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+    // canActivate: [AuthGardService]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'extract-list',
+    component: ExtractListComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
 ];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        RegistrationComponent,
-        LoginComponent,
-        ExtractListComponent,
-        ConfirmDownloadDialogComponent
-    ],
-    imports: [
-        RouterModule.forRoot(
-            appRoutes,
-            {enableTracing: true} // <-- debugging purposes only
-        ),
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        FormsModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        FileSaverModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatMenuModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatInputModule,
-        MatSelectModule,
-        MatTableModule,
-        MatSortModule,
-        MatCheckboxModule,
-        MatProgressSpinnerModule,
-        MatDialogModule,
-        RouterModule
-    ],
-    entryComponents: [
-        ConfirmDownloadDialogComponent
-    ],
-    providers: [
-        SubscribersService,
-        SignInService,
-        RegistrationService,
-        {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
-        {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
-        {provide: HTTP_INTERCEPTORS, useClass: ErrorLogoutInterceptor, multi: true}],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ExtractListComponent,
+    ConfirmDownloadDialogComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FileSaverModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    RouterModule
+  ],
+  entryComponents: [
+    ConfirmDownloadDialogComponent
+  ],
+  providers: [
+    SubscribersService,
+    SignInService,
+    RegistrationService,
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorLogoutInterceptor, multi: true}],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
