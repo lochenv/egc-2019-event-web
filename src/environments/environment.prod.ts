47d7483e2a6f2fab3d27a3431b87457a05fbe058
Vincent <lochenv@gmail.com>
2019-07-06 18:01:46 +0200

    =Finalize registration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,5 @@ export const environment = {
 };
 
 export const javaUri = 'https://egc-2019-event.herokuapp.com/api/';
+export const phpUri = 'http://testng.egc2019.eu/subscribers/';
+export const egdUri = 'http://www.europeangodatabase.eu/EGD/';
